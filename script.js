5c1c9b18afa1a35bb972dd7163f217e07f400688
Lawson812 <lawson.james.e@gmail.com>
2018-10-02 13:58:43 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+ 
+  // preloader
+  $(window).load(function(){
+    $('.preloader').delay(1200).fadeOut(200);
+  })
+	
+
+})
+ ScrollReveal().reveal('.photos', {delay:200});
